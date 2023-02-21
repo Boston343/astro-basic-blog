86541690ac76bbfb6be334f59d3b356266459945
Boston343 <branden.austin1@gmail.com>
2023-02-21 18:56:23 -0500

    Lesson 14 - done - sitemap generation implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:3000",
   integrations: [
     react(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
